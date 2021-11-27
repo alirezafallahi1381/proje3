1094b0431cd6e54fdd2b670b66e0cea991999c3f
Fallahi <al>
2021-11-27 00:16:42 -0800

    Initial commit

---
+++ app/src/main/java/com/example/proje3/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.proje3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var web_view: WebView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        web_view = findViewById<WebView>(R.id.ac_main_webview)
+        web_view.loadUrl("https://www.digikala.com/")
+        web_view.webViewClient = WebViewClient()
+    }
+    override fun onBackPressed() {
+        if(web_view.canGoBack()){
+            web_view.goBack()
+        }else{
+            super.onBackPressed()
+        }
+    }
+}
